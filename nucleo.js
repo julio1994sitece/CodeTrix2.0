c75b8cbe1c12e2bcb46b9c25db6bcbc138a0c0e8
julio1994sitece <75703510+julio1994sitece@users.noreply.github.com>
2021-08-09 10:57:15 -0300

    Add files via upload

---
+++ nucleo.js
@@ -0,0 +1,9 @@
+document.getElementById("cadastro").style.display="none";
+document.getElementById("subcadastro").style.display="block";
+document.getElementById("a01").style.display="block";
+
+
+function app(){
+         document.getElementById("cadastro").style.display="block";
+         document.getElementById("a01").style.display="none";
+         }
